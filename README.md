2946598aa26e7b0c9b2a44a9231fe10b3f51b0f8
Joel Takvorian <jtakvori@redhat.com>
2018-05-21 15:22:13 +0200

    Better ball physics

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # demo-mesh-arena
+
+## How-to
+
+- Run UI's main
+- Open browser to localhost:8080
+- Run Ball's main
+- ```curl -H "Content-Type: application/json" -X PUT -d '{"dx":200,"dy":10}' http://localhost:8081/shoot```
--- services/ball/src/main/java/demo/mesharena/ball/Ball.java
+++ services/ball/src/main/java/demo/mesharena/ball/Ball.java
@@ -12,10 +12,14 @@ import io.vertx.ext.web.RoutingContext;
 
 public class Ball extends AbstractVerticle {
 
+  private static final long DELTA_MS = 100;
+  private static final double RESISTANCE = 80;
+
   private double x;
   private double y;
   private double dx;
   private double dy;
+  private boolean isSticked;
 
   private Ball() {
   }
@@ -51,7 +55,7 @@ public class Ball extends AbstractVerticle {
     vertx.createHttpServer().requestHandler(router::accept).listen(8081);
 
     // Start game loop
-    vertx.setPeriodic(200, loopId -> this.update(0.2));
+    vertx.setPeriodic(DELTA_MS, loopId -> this.update((double)DELTA_MS / 1000.0));
   }
 
   private void set(RoutingContext ctx) {
@@ -77,17 +81,31 @@ public class Ball extends AbstractVerticle {
   }
 
   private void update(double delta) {
-    x += delta * dx;
-    y += delta * dy;
-    display();
-    // TODO: resistance (decrease absolute derivate)
+    double oldX = x;
+    double oldY = y;
+    if (isSticked) {
+      // TODO: special movement when a player owns the ball
+    } else if (delta > 0) {
+      x += delta * dx;
+      y += delta * dy;
+      double oldNorm = vecSize(dx, dy);
+      double newNorm = Math.max(0, oldNorm - RESISTANCE * delta);
+      // TODO: check wall collision
+      if (oldNorm == 0) {
+        dx = dy = 0;
+      } else {
+        dx = dx * newNorm / oldNorm;
+        dy = dy * newNorm / oldNorm;
+      }
+    }
+    if (oldX != x || oldY != y) {
+      display();
+    }
   }
 
   private void display() {
     HttpClient client = vertx.createHttpClient(new HttpClientOptions().setDefaultHost("localhost").setDefaultPort(8080));
-    HttpClientRequest request = client.request(HttpMethod.POST, "/movegameobject", response -> {
-      System.out.println("Received response with status code " + response.statusCode());
-    });
+    HttpClientRequest request = client.request(HttpMethod.POST, "/movegameobject", response -> {});
 
     // Register game object
     String json = new JsonObject()
@@ -99,5 +117,9 @@ public class Ball extends AbstractVerticle {
     request.putHeader("content-length", String.valueOf(json.length()));
     request.write(json);
     request.end();
+  }
+
+  private static double vecSize(double x, double y) {
+    return Math.sqrt(x*x+y*y);
   }
 }
