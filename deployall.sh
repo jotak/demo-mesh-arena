7df8d3bc2ac6261901eb102f996164a217f0b30a
Joel Takvorian <jtakvori@redhat.com>
2019-11-13 18:09:58 +0100

    Fix some yaml (env strings)

--- deployall.sh
+++ deployall.sh
@@ -5,5 +5,7 @@ oc project mesh-arena
 oc adm policy add-scc-to-user privileged -z default
 
 oc apply -f <(istioctl kube-inject -f full-metrics.yml)
+# oc label namespace mesh-arena istio-injection=enabled
+# oc apply -f full-metrics.yml
 
 oc expose service ui
