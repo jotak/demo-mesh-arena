a1fee404e38dc24d783c06bddad7a742cca1fcae
Joel Takvorian <jtakvori@redhat.com>
2020-07-16 18:48:21 +0200

    Update for istio 1.6 assuming flag meshConfig.enablePrometheusMerge=true

--- kdeployall.sh
+++ kdeployall.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
+kubectl label namespace default istio-injection=enabled
+
 if [[ "$1" = "metrics" ]]; then
-    cat full.yml | ./with_metrics.sh | istioctl kube-inject -f - | kubectl apply -f -
+    cat full.yml | ./with_metrics.sh | kubectl apply -n default -f -
     exit
 fi
 
 if [[ "$1" = "tracing" ]]; then
-    cat full.yml | ./with_tracing.sh | istioctl kube-inject -f - | kubectl apply -f -
+    cat full.yml | ./with_tracing.sh | kubectl apply -n default -f -
     exit
 fi
 
 if [[ "$1" = "both" ]]; then
-    cat full.yml | ./with_metrics.sh | ./with_tracing.sh | istioctl kube-inject -f - | kubectl apply -f -
+    cat full.yml | ./with_metrics.sh | ./with_tracing.sh | kubectl -n default apply -f -
     exit
 fi
 
 if [[ "$1" = "naked" ]]; then
-    cat full.yml | istioctl kube-inject -f - | kubectl apply -f -
+    cat full.yml | kubectl -n default apply -f -
     exit
 fi
 
