03094f599156773861ebb119a868bffc3de334cb
Joel Takvorian <jtakvori@redhat.com>
2025-02-11 12:34:34 +0100

    add dday2022 info

--- scenario/dday2022.mk
+++ scenario/dday2022.mk
@@ -3,6 +3,9 @@ dday-init-stadium: scen-init-stadium
 dday-init-ball: scen-init-ball
 dday-init-players: scen-init-players
 
+dday-port-forward:
+	xdg-open http://localhost:8080/ && kubectl port-forward -n ${NAMESPACE} svc/ui 8080
+
 dday-22-players:
 	kubectl scale deployment -n ${NAMESPACE} player-locals --replicas=11
 	kubectl scale deployment -n ${NAMESPACE} player-visitors --replicas=11
@@ -17,11 +20,19 @@ dday-hack-maradona:
 dday-unhack:
 	kubectl delete namespace hacker
 
-dday-setup-policy:
-	kubectl apply -n ${NAMESPACE} -f ./scenario/dday2022/network-policy.yaml
+dday-setup-namespace-policy:
+	kubectl apply -n ${NAMESPACE} -f ./scenario/dday2022/network-policy-1.yaml
+	kubectl delete pods -n ${NAMESPACE} -l app=ui
+	kubectl delete pods -n ${NAMESPACE} -l app=ball
+
+dday-setup-final-policy:
+	kubectl apply -n ${NAMESPACE} -f ./scenario/dday2022/network-policy-2.yaml
+	kubectl delete pods -n ${NAMESPACE} -l app=ui
+	kubectl delete pods -n ${NAMESPACE} -l app=ball
 
 dday-delete-policy:
 	kubectl delete -n ${NAMESPACE} -f ./scenario/dday2022/network-policy.yaml
 
 dday-undeploy: undeploy
 	kubectl delete --ignore-not-found namespace hacker
+	kubectl delete --ignore-not-found namespace ${NAMESPACE}
